35bee62e7baa0c7b3c7ddb7886f507b1633cecc6
CheongYa <chch1234569@naver.com>
2022-10-03 21:37:18 +0900

    feat(baseball): create skeleton code

---
+++ src/main/java/baseball/domain/NumberGenerator.java
@@ -0,0 +1,10 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class NumberGenerator {
+    //List = java에서 기본적으로 제공해주는 기능
+    public List<Integer> createRandomNumbers() {
+        return null;
+    }
+}
