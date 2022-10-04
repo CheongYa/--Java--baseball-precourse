e7f073cf57b82b1ad92fd1878db34ea8e21c92fc
CheongYa <chch1234569@naver.com>
2022-10-04 12:40:19 +0900

    feat(baseball): 아웃 구현

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -17,6 +17,10 @@ public class Referee {
             }
         }
         int ball = correntCount - strike;
+
+        if (strike == 0){
+            return "아웃";
+        }
         return ball + " 볼 " + strike + " 스트라이크";
     }
 }
